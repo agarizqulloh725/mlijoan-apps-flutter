972628d218bd9c0e96aa0e1ca6efc09972e4a4ec
Aga Rizqulloh <2377anr@gmail.com>
2024-12-22 08:55:56 +0700

    done : whitlist 50%

--- lib/pages/wishlist_page.dart
+++ lib/pages/wishlist_page.dart
@@ -1,10 +1,74 @@
 import 'package:flutter/material.dart';
 
-class WishlistPage extends StatelessWidget {
+class WishlistPage extends StatefulWidget {
   const WishlistPage({super.key});
 
   @override
+  State<WishlistPage> createState() => _WishlistPageState();
+}
+
+class _WishlistPageState extends State<WishlistPage> {
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
   Widget build(BuildContext context) {
-    return Center(child: const Text('Wishlist Page'));
+    return Column(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Container(
+            width: double.infinity,
+            height: 50.0,
+            color: Colors.white,
+            padding: const EdgeInsetsDirectional.fromSTEB(20.0, 0.0, 20.0, 0.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  '890 Barang',
+                  style: Theme.of(context).textTheme.bodyMedium,
+                ),
+                const Icon(
+                  Icons.view_list_sharp,
+                  size: 24.0,
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 10.0,
+                mainAxisSpacing: 10.0,
+                childAspectRatio: 1.0,
+              ),
+              itemCount: 10,
+              itemBuilder: (context, index) {
+                return const _WishlistItem();
+              },
+            ),
+          ),
+        ],
+    );
+  }
+}
+
+class _WishlistItem extends StatelessWidget {
+  const _WishlistItem();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Expanded(
+          child: Image.network(
+            'https://picsum.photos/seed/113/600',
+            fit: BoxFit.cover,
+          ),
+        ),
+      ],
+    );
   }
 }
